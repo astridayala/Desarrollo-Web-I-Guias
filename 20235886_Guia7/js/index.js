592460140bc2fa1966f2d46b05e2b7bc01ae961c
astridayala <astriayala06@gmail.com>
2024-11-04 19:19:01 -0600

    detalles

--- 20235886_Guia7/js/index.js
+++ 20235886_Guia7/js/index.js
@@ -1,32 +1,32 @@
 //referencia de los botones
-const buttonSpan = document.getElementById("idBtnSpan")
-const buttonP = document.getElementById("idBtnP")
-const buttonDiv = document.getElementById("idBtnDiv")
-const buttonButton = document.getElementById("idBtnButton")
-const imprimir = document.getElementById("idImprimirResultado")
+const buttonSpan = document.getElementById("idBtnSpan");
+const buttonP = document.getElementById("idBtnP");
+const buttonDiv = document.getElementById("idBtnDiv");
+const buttonButton = document.getElementById("idBtnButton");
+const imprimir = document.getElementById("idImprimirResultado");
 
 //contar elementos dentro del HTML
 const contarElementos = function(elemento){
     //obtener todas las etiquetas span
-    let arrayElement = document.getElementsByTagName(elemento)
+    let arrayElement = document.getElementsByTagName(elemento);
     console.log(
         `Etiquetas buscadas <${elemento}><${elemento}> / Total encontradas: ${arrayElement.length}`
-    )
+    );
     for (const i of arrayElement){
-        console.log(i)
+        console.log(i);
     }
-    alert("Revise la consola del navegador")
-}
+    alert("Revise la consola del navegador");
+};
 //eventos para los botones
 buttonSpan.onclick = () => {
-    contarElementos("span")
-}
+    contarElementos("span");
+};
 buttonP.onclick = () => {
-    contarElementos("p")
-}
+    contarElementos("p");
+};
 buttonDiv.onclick = () => {
-    contarElementos("div")
-}
+    contarElementos("div");
+};
 buttonButton.onclick = () => {
-    contarElementos("button")
+    contarElementos("button");
 }
